401f748a6bf9a9edd89cfa93e4e9fb1d5ed6532e
pattytonyoneill <pattytonyoneill@gmail.com>
2022-07-28 13:56:49 +0000

    Added home app and templates

--- caitlins_crafts/settings.py
+++ caitlins_crafts/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +47,10 @@ ROOT_URLCONF = 'caitlins_crafts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
