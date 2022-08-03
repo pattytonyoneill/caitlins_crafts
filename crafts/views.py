e8230eebac1f45fee2be9487e1fbf028c0179f5a
pattytonyoneill <pattytonyoneill@gmail.com>
2022-08-03 16:06:52 +0000

    Add search functionality

--- crafts/views.py
+++ crafts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Crafts
 
 
@@ -6,9 +8,21 @@ def all_crafts(request):
     """ A view to show all crafts, including sorting and search queries """
 
     crafts = Crafts.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('crafts'))
+          
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            crafts = crafts.filter(queries)
 
     context = {
         'crafts': crafts,
+        'search_term': query,
     }
 
     return render(request, 'crafts/crafts.html', context)
