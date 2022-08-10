d79751a4a4f3f2efbb5fc5ac53fd42fa1308220c
pattytonyoneill <pattytonyoneill@gmail.com>
2022-08-10 10:53:46 +0000

    Finish Product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,15 +12,27 @@ def bag_contents(request):
    
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        craft = get_object_or_404(Crafts, pk=item_id)
-        total += quantity * craft.price
-        craft_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'craft': craft,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            craft = get_object_or_404(Crafts, pk=item_id)
+            total += item_data * craft.price
+            craft_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'craft': craft,
+            })
+        else:
+            craft = get_object_or_404(Crafts, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * craft.price
+                craft_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'craft': craft,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
