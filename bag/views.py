61062ae8104def19b1990db361047c3fd13e627f
pattytonyoneill <pattytonyoneill@gmail.com>
2022-08-17 13:08:30 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from crafts.models import Crafts
 
 
 def view_bag(request):
@@ -10,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified craft to the shopping bag """
 
+    craft = Crafts.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -32,6 +35,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {craft.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
