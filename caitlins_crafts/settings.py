2c672773b0adbcd9ef5675877aa731909675a379
pattytonyoneill <pattytonyoneill@gmail.com>
2022-08-18 14:45:10 +0000

    Add checkout views and templates

--- caitlins_crafts/settings.py
+++ caitlins_crafts/settings.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     'crafts',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +49,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'caitlins_crafts.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -61,7 +66,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required  allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
--- checkout/models.py
+++ checkout/models.py
@@ -61,9 +61,10 @@ class OrderLineItem(models.Model):
     order = models.ForeignKey(
         Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     craft = models.ForeignKey(Crafts, null=False, blank=False, on_delete=models.CASCADE)
-    craft_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    craft_size = models.CharField(max_length=2, null=True, blank=True)  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
