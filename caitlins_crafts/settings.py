155e25d452f555b2712bab9cb89d1ae18d09863b
pattytonyoneill <pattytonyoneill@gmail.com>
2022-08-23 13:35:39 +0000

    Settings.py added env.py details to

--- caitlins_crafts/settings.py
+++ caitlins_crafts/settings.py
@@ -1,6 +1,9 @@
 import os
 import dj_database_url
 
+if os.path.exists('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
