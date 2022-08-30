90dc5bc08a954f54456147047106d860773693ae
pattytonyoneill <pattytonyoneill@gmail.com>
2022-08-30 14:01:09 +0000

    Fix thhe image field

--- crafts/forms.py
+++ crafts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Crafts, Category
 
 
@@ -7,6 +8,8 @@ class CraftForm(forms.ModelForm):
     class Meta:
         model = Crafts
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
