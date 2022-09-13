4122cc7cf5bffaa738f130a1eef6dc7b95f51950
pattytonyoneill <pattytonyoneill@gmail.com>
2022-09-13 12:31:37 +0000

    Add newsletter and contact urls

--- caitlins_crafts/urls.py
+++ caitlins_crafts/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('newsletter/', include('newsletter.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
